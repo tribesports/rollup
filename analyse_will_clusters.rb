b2b67bbe60bf0aa5f4822426c29b2772ed3b695c
Tom Stuart <tom@therye.org>
2012-07-30 10:08:50 +0100

    Script to compare canopy clusterer output vs will's

--- analyse_will_clusters.rb
+++ analyse_will_clusters.rb
@@ -4,23 +4,51 @@ $:.unshift File.expand_path("../lib", __FILE__)
 
 require 'rollup'
 
-class MeanDistanceAnalyser
-  attr_reader :average_distances
-  def initialize
-    @average_distances = []
-  end
+debug = (ARGV[2] == "debug")
 
-  def start(stuff)
+analyzer = Rollup::TextAnalyzer.new(Version::LUCENE_36) do |a|
+  h = HTMLEntities.new
+  a.pre_processor do |text|
+    h.decode(text).gsub(Rollup::Runner::STOP_WORDS_REGEX, "")
   end
+  a.filter StandardFilter
+  a.filter LowerCaseFilter
+  a.filter(StopFilter) {|token_stream| [a.version, token_stream, StandardAnalyzer::STOP_WORDS_SET] }
+  #      a.filter PorterStemFilter
+end
+dictionary = Rollup::WeightedDictionary.new
+text_encoder = StaticWordValueEncoder.new("text")
 
-  def cluster(cluster_info)
-    @average_distances << cluster_info.average_distance
-  end
+sentence_vector_maker = Rollup::SentenceVectorMaker.new(analyzer, dictionary, text_encoder)
+examples = CSV.read(ARGV[0]).map {|v| v[2] } #get them in groups
+examples.each {|e| sentence_vector_maker.add_example(e) }
+group_collector = Rollup::Outputs::GroupedProductOutput.new
+Rollup::Inputs::CSVGroupImport.new(group_collector).run(ARGV[0])
+groups_with_more_than_one = group_collector.groups.group_by(&:first).select {|cat, products| products.size > 1 }
+interesting_groups = groups_with_more_than_one.reject {|cat, products| products.all? {|p| p[2] == products.first[2] }}
+averages = group_collector.groups.group_by(&:first).map do |cat, products|
+  examples = products.map {|p| p[2] }
+  sentence_vector_maker.average_distance(examples)
+end
+puts "Will's samples, average distance:"
+puts averages.inject(:+) / averages.size
 
-  def finish
-  end
+actual_group_collector = Rollup::Outputs::GroupedProductOutput.new
+Rollup::Runner.new(actual_group_collector).run(ARGV[1])
+actual_groups_with_more_than_one = actual_group_collector.groups.group_by(&:first).select {|cat, products| products.size > 1 }
+actual_interesting_groups = actual_groups_with_more_than_one.reject {|cat, products| products.all? {|p| p[2] == products.first[2] }}
+actual_averages = actual_group_collector.groups.group_by(&:first).map do |cat, products|
+  examples = products.map {|p| p[2] }
+  sentence_vector_maker.average_distance(examples)
 end
 
-analyser = MeanDistanceAnalyser.new
-Rollup::Runner.new(analyser).run(ARGV[0], false)
-puts analyser.average_distances.inspect
+puts "Actual output, average distance:"
+puts actual_averages.inject(:+) / actual_averages.size
+
+if debug
+  puts "Interesting groups in will clusters:"
+  puts interesting_groups.map {|cat, products| products.map {|p| p[2]}.join(", ") + " (#{products.size})"}.join("\n\n")
+
+  puts "Interesting groups in actual clusters:"
+  puts actual_interesting_groups.map {|cat, products| products.map {|p| p[2]}.join(", ") + " (#{products.size})"}.join("\n\n")
+end
--- lib/rollup/sentence_vector_maker.rb
+++ lib/rollup/sentence_vector_maker.rb
@@ -1,18 +1,34 @@
 require 'java'
 import org.apache.mahout.math.RandomAccessSparseVector
 
-class SentenceVectorMaker
-  def initialize(analyzer, dictionary, text_encoder)
-    @analyzer = analyzer
-    @dictionary = dictionary
-    @text_encoder = text_encoder
-  end
-
-  def vector_for(example, features)
-    vector = RandomAccessSparseVector.new(features)
-    @analyzer.each_token_for(example) do |token|
-      @text_encoder.add_to_vector(token, @dictionary.weight(token), vector)
+module Rollup
+  class SentenceVectorMaker
+    def initialize(analyzer, dictionary, text_encoder)
+      @analyzer = analyzer
+      @dictionary = dictionary
+      @text_encoder = text_encoder
+      @examples = []
     end
-    return vector.normalize
+
+    def add_example(example)
+      @examples << example
+      @analyzer.each_token_for(example) do |token|
+        @dictionary.add(token)
+      end
+    end
+
+    def vector_for(example, features)
+      vector = RandomAccessSparseVector.new(features)
+      @analyzer.each_token_for(example) do |token|
+        @text_encoder.add_to_vector(token, @dictionary.weight(token), vector)
+      end
+      vector.normalize
+    end
+
+    def average_distance(examples)
+      examples.combination(2).inject(0.0) do |acc, (a, b)|
+        acc + (vector_for(a, 100000).minus(vector_for(b, 100000)).get_length_squared) ** 0.5
+      end / examples.length
+    end
   end
 end
